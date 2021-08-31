f8d70a385dbd4c94b9d0227a2bbb5925b687f01e
derekshum <derekqlshum@gmail.com>
2021-08-31 00:48:45 -0400

    Adding poping root
    Can now sort.

--- HeapPractice/Program.cs
+++ HeapPractice/Program.cs
@@ -26,8 +26,34 @@ namespace HeapPractice
                 swap(currentIndex, (currentIndex - 1) / 2);
                 currentIndex = (currentIndex - 1) / 2;
             }
-                
+        }
 
+        public int PopRoot()
+        {
+            int root = heap[0];
+            heap[0] = heap[heap.Count - 1];
+            heap.RemoveAt(heap.Count - 1);
+            int currentIndex = 0;
+            while (currentIndex * 2 + 1 <= heap.Count - 1)  //while current index still has a child
+            {
+                int leftIndex = currentIndex * 2 + 1;
+                int rightIndex = Math.Min(currentIndex * 2 + 2, heap.Count - 1);    //allows for comparison to just left root if only left root exists
+                
+                if (heap[currentIndex] >= Math.Max(heap[leftIndex], heap[rightIndex]))
+                    break;
+                
+                if (heap[leftIndex] >= heap[rightIndex])
+                {
+                    swap(currentIndex, leftIndex);
+                    currentIndex = leftIndex;
+                }
+                else
+                {
+                    swap(currentIndex, rightIndex);
+                    currentIndex = rightIndex;
+                }
+            }
+            return root;
         }
 
         private void swap(int index1, int index2)
@@ -49,8 +75,14 @@ namespace HeapPractice
             for (int i = 0; i < 10; i++)
             {
                 int entry = r.Next(0, 99);
-                Console.WriteLine(entry);
+                Console.WriteLine("Adding " + entry);
                 test.Insert(entry);
+                printHeap(test.heap);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Removing " + test.PopRoot());
                 printHeap(test.heap);
             }
         }
